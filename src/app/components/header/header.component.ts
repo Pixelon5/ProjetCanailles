2c4234ef1d8dc80aff689b89cdd3ed91040a1b21
Pixelon5 <thibault.beteille@gmail.com>
2020-04-20 00:17:35 +0200

    sliding underline on header links and twitter replaced by instagram

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild('mainMenuUnderline') mainMenuUnderline: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      const liActive = document.querySelector('.header-area .main-menu ul li a.active').parentElement;
+      const ul = liActive.parentElement;
+      this.mainMenuUnderline.nativeElement.style.opacity = 1;
+      this.mainMenuUnderline.nativeElement.style.marginLeft = (Array.from(ul.children).indexOf(liActive) * 25).toString() + '%';
+    });
   }
-
 }
